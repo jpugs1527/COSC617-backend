229a0c732c052cbca7c68d5578b2788401558534
Jordan Puglisi <jpugli1@students.towson.edu>
2020-04-03 17:31:06 -0400

    add .env and setup mongodb atlas

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ var express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const { Connection } = require('./lib/mongoConnection');
+
+// Establishing db connection
+Connection.connectToMongo();
+console.log(Connection);
 
 // Tell app.js where to get the controllers
-const userController = require
+const userController = require('./controllers/userController');
 
 app.get('/', (req, res) => {
-  res.send("hello world");
+  res.json({text:"hello world"});
 })
 
-app.use('user', userController);
+app.use('/user', userController);
 
-app.listen(3001);
+app.listen(3000);
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,14 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
-/* GET index page. */
-router.get('/', function (req, res, next) {
-  res.send("Hello User");
+/* Return all users in the db. */
+router.get('/all', function (req, res, next) {
+  res.send("Hellasddo Uhsefr");
+});
+
+// Create a new user
+router.post('/new', function (req, res, next) {
+  
 });
 
 module.exports = router;
